3f23fdce04532438d21fca94d80957c2c00cde08
Tipuch <fiorini751@proton.me>
2024-09-27 20:32:31 +0700

    tyding up the config stuff

--- config/config.py
+++ config/config.py
@@ -1,2 +1,22 @@
+import os
+
+from fastapi.templating import Jinja2Templates
+from inertia import InertiaConfig
+
+
 class Config:
-    pass
+    SECRET_KEY = os.environ["SECRET_KEY"]
+
+    TEMPLATES_DIR = os.path.join(os.path.dirname(__file__), "templates")
+
+    MANIFEST_JSON = os.path.join(
+        os.path.dirname(__file__), "static", "dist", "manifest.json"
+    )
+
+    SVELTE_DIR = os.path.join(os.path.dirname(__file__), "static", "src")
+
+    def get_templates(self):
+        return Jinja2Templates(directory=self.TEMPLATES_DIR)
+
+    def get_inertia_config(self) -> InertiaConfig:
+        raise NotImplementedError()
--- main.py
+++ main.py
@@ -8,17 +8,11 @@ from inertia import (
     inertia_dependency_factory,
     InertiaResponse,
     inertia_request_validation_exception_handler,
-    InertiaConfig,
 )
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi import FastAPI, Depends
-from fastapi.templating import Jinja2Templates
 from config.dev import DevConfig
-
-
-template_dir = os.path.join(os.path.dirname(__file__), "templates")
-templates = Jinja2Templates(directory=template_dir)
 
 
 def get_config():
@@ -39,29 +33,17 @@ app.add_exception_handler(
     RequestValidationError,
     inertia_request_validation_exception_handler,
 )
-app.add_middleware(SessionMiddleware, secret_key="secret_key")
+app.add_middleware(SessionMiddleware, secret_key=config.SECRET_KEY)
 
-manifest_json = os.path.join(
-    os.path.dirname(__file__), "static", "dist", "manifest.json"
-)
+InertiaDep = Annotated[
+    Inertia, Depends(inertia_dependency_factory(config.get_inertia_config()))
+]
 
-inertia_config = InertiaConfig(
-    templates=templates,
-    manifest_json_path=manifest_json,
-    environment="development",
-    use_flash_messages=True,
-    use_flash_errors=True,
-    entrypoint_filename="main.js",
-    assets_prefix="/src",
-)
-
-InertiaDep = Annotated[Inertia, Depends(inertia_dependency_factory(inertia_config))]
-
-svelte_dir = os.path.join(os.path.dirname(__file__), "static", "src")
-
-app.mount("/src", StaticFiles(directory=svelte_dir), name="src")
+app.mount("/src", StaticFiles(directory=config.SVELTE_DIR), name="src")
 app.mount(
-    "/assets", StaticFiles(directory=os.path.join(svelte_dir, "assets")), name="assets"
+    "/assets",
+    StaticFiles(directory=os.path.join(config.SVELTE_DIR, "assets")),
+    name="assets",
 )
 
 
